54cd642ce568b4e486aae7eab78f93d933240204
Potsawat Khunkum <aikahotdeath@gmail.com>
2019-07-13 14:13:34 +0700

    add test

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Potsawat Khunkum'))
+app.get('/', (req, res) => res.send('หน้า home'))
 app.get('/about', (req, res) => res.send('หน้า about'))
 app.get('/contact', (req, res) => res.send('หน้า contact'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
